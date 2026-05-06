54059a5b6a0f362c97f4432c8f5800788b4ea09e
ShortVincentMan <vinvyle06@gmail.com>
2026-05-05 17:04:59 -0700

    fix: goal_indicator missed method calls

--- Assets/Scripts/goal_Indicator.cs
+++ Assets/Scripts/goal_Indicator.cs
@@ -32,12 +32,14 @@ public class Goal : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
+        {
+            HandleLevelComplete();
             StartCoroutine(ShowAndHide());
+        }
 
-            // I was thinking we can do like a time delay as done in ShowAndHide
-            // And then move to the next level
-            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
+        // I was thinking we can do like a time delay as done in ShowAndHide
+        // And then move to the next level
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void OnMouseDown()
